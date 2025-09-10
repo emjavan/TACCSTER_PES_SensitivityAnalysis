287d60cf829c91f7c39a5b474a47d7eee496f31e
Emily Javan, MS PhD <emjavan@gmail.com>
2025-09-10 14:18:58 -0500

    R0 sensitivity launcher files

---
+++ R0_sensitivity_analysis/r0_sensitivity_job_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH   -p development
+#SBATCH   -J r0_sensitivity
+#SBATCH   -o r0_sensitivity.o%j
+#SBATCH   -e r0_sensitivity.o%j
+#SBATCH   –ntasks-per-node 1                # this parameter is ignored
+#SBATCH   -N 2
+#SBATCH   -t 0:40:00
+#SBATCH   -A  TACC-SCI
+#SBATCH   --mail-user=emjavan@utexas.edu    # Email for notifications
+#SBATCH   --mail-type=all                   # Type of notifications, begin, end, fail, all
+
+poetry install --no-root
+module load python3
+python3 example_classic_launcher.py
---
+++ R0_sensitivity_analysis/r0_sensitivity_launcher.py
@@ -0,0 +1,2 @@
+import pylauncher
+pylauncher.ClassicLauncher("r0_sensitivity_commands.txt",debug="host+job")
