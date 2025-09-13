e92b452d4f122d83e6955b794329783352b15cb0
Emily Javan, MS PhD <emjavan@gmail.com>
2025-09-13 09:05:50 -0500

    update for full job (10K sims)

--- R0_sensitivity_analysis/r0_sensitivity_job_script.sh
+++ R0_sensitivity_analysis/r0_sensitivity_job_script.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-#SBATCH   -p development
-#SBATCH   -J r0_sensitivity
-#SBATCH   -o r0_sensitivity.o%j
-#SBATCH   -e r0_sensitivity.o%j
-#SBATCH   --ntasks-per-node 1                # this parameter is ignored
-#SBATCH   -N 2
-#SBATCH   -t 0:40:00
-#SBATCH   -A  TACC-SCI
+#SBATCH   -p normal
+#SBATCH   -J full_r0_sens
+#SBATCH   -o full_r0_sens.o%j
+#SBATCH   -e full_r0_sens.o%j
+#SBATCH   --ntasks-per-node 1               # this parameter is ignored
+#SBATCH   -N 3
+#SBATCH   -t 6:00:00
+#SBATCH   -A TACC-SCI
 #SBATCH   --mail-user=emjavan@utexas.edu    # Email for notifications
 #SBATCH   --mail-type=all                   # Type of notifications, begin, end, fail, all
 
 # Only have to run once when git repo just cloned
 #poetry install --no-root
-
 
 module load python3
 module load pylauncher
