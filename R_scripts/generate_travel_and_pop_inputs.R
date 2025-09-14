701d66625d7e0a0dd851bb260cc0274e7540c800
Emily Javan, MS PhD <emjavan@gmail.com>
2025-09-14 18:52:42 -0500

    fips were not unique

--- R_scripts/generate_travel_and_pop_inputs.R
+++ R_scripts/generate_travel_and_pop_inputs.R
@@ -26,22 +26,27 @@ round_preserve_sum_char <- function(x, target_sum) {
   return(comma_separated_string)
 } # end round_preserve_sum_char
 
+# Generate n unique 5-digit FIPS-like IDs (character)
+# e.g., start=10000 -> "10000","10001","10002",...
+generate_fips_5digit = function(n, start = 10000L) {
+  stopifnot(n >= 1, start >= 0, start + n - 1 <= 99999)
+  sprintf("%05d", seq.int(from = start, length.out = n))
+}
+
 # 1M is hard coded in name output which will be issue if this value changes
-pop_size     = 1000000 
+pop_size     = 1000000 # 1M
 age_grp_labs = c("0-4","5-17","18-49","50-64","65+")
 num_age_grp  = length(age_grp_labs)
 num_nodes    = c(2, 10, 100, 250) # made 1 node by hand but could add
-base_fips    = 00000
 age_props    = rep(1/length(age_grp_labs), length(age_grp_labs))
 
-for (n in num_nodes) {
+for(n in num_nodes){
   out_dir = file.path("..", "data", paste0("Network_", n, "_Node"))
   if(!dir.exists(out_dir)){dir_create(out_dir, recurse = TRUE)}
   
   # fips: base_fips, base_fips+10000, ... up to num nodes and at most length 5
-  fips = base_fips + 10000L * (0:(n - 1L))
-  fips = as.character(str_sub(fips, 1, 5))
-  fips = str_pad(fips, 5, "left", pad="0")
+  fips = generate_fips_5digit(n)
+  stopifnot(anyDuplicated(fips) == 0)
   
   # ---- county_age_matrix_1M.csv ----
   # total people per node (≈ pop_size/n, adjust for rounding)
