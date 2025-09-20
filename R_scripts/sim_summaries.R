46f7841756eaffb86100f248825cecbec78ddbfe
Emily Javan, MS PhD <emjavan@gmail.com>
2025-09-20 00:41:07 -0500

    print input

--- R_scripts/sim_summaries.R
+++ R_scripts/sim_summaries.R
@@ -6,11 +6,12 @@ library(tidyverse)
 source("TACCSTER_fig_fns.R")
 input_dir_path = "../R0_sensitivity_analysis/"
 
-args <- commandArgs()
-network_size = as.integer(args[[1]])
-batch_num    = as.integer(args[[2]])
-start        = as.integer(args[[3]])
-end          = as.integer(args[[4]])
+args <- commandArgs(trailingOnly = TRUE)
+network_size = as.integer(args[[1]]); print(network_size)
+batch_num    = as.integer(args[[2]]); print(batch_num)
+start        = as.integer(args[[3]]); print(start)
+end          = as.integer(args[[4]]); print(end)
+
 
 sims = read_csv(paste0(input_dir_path, "sim_output_paths_Network_", network_size, "_Nodes-seatird-r01-sim0_99.csv"))
 sim_sub = sims %>%
